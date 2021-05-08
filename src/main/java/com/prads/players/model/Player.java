c5b6835cefb230790e41aeda4a1b36d86f40f77b
Leandro Pradal <le.pradal.lopes@gmail.com>
2021-05-08 16:17:58 -0300

    Criação da classe Player - Somente atributos

---
+++ src/main/java/com/prads/players/model/Player.java
@@ -0,0 +1,19 @@
+package com.prads.players.model;
+
+public class Player {
+
+    public Long id;
+    public String nick;
+    public String name;
+    public int level;
+    public int exp;
+    public int matches;
+    public Double kdr;
+    public int kill;
+    public int death;
+    public Double headShotPerc;
+
+    public Player() {
+
+    }
+}
